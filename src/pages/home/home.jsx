7b97d32a474ee415f516e4a05c428abd89e34499
Francisco <figueroaf@moravian.edu>
2024-05-01 22:42:42 -0400

    started working on media queries

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,5 +1,5 @@
 import './home.css';
-import { Users, DownloadSimple, Coins, Bank, CurrencyBtc, Money,CurrencyEth,CurrencyCircleDollar,ShieldCheck,Lock,Wallet} from 'phosphor-react';
+import { Users, DownloadSimple, Coins, Bank, CurrencyBtc, Money,CurrencyEth,CurrencyCircleDollar,ShieldCheck,Lock,Wallet, GooglePlayLogo} from 'phosphor-react';
 import { useEffect } from 'react';
 import mainLogo from '../../img/logo.png'
 
@@ -27,7 +27,7 @@ export const Home = () => {
                             Get Started
                         </button>
                         <button className='home-download-bttn'>
-                             Download For IOS
+                            <GooglePlayLogo color='white'  /> Download For IOS
                         </button>
                     </div>
                     <div className='home-active-info'>
@@ -45,15 +45,15 @@ export const Home = () => {
 
 
                 <div className='home-app'>
-                    <div className="home-item1 hidden"><h2><Money size={64} /></h2></div>
-                    <div className="home-item2 hidden2"><h2><Coins size={64}  /></h2></div>
-                    <div className="home-item3 hidden3"><h2><Bank size={64} /></h2></div>
-                    <div className="home-item4 hidden4"><h2><CurrencyBtc size={64} /></h2></div>
-                    <div className="home-item5 hidden5"><h2><CurrencyEth size={64} /></h2></div>
-                    <div className="home-item6 hidden6"><h2><CurrencyCircleDollar size={64} /></h2></div>
-                    <div className="home-item7 hidden7"><h2>< ShieldCheck size={64} /></h2></div>
-                    <div className="home-item8 hidden8"><h2><Lock size={64} /></h2></div>
-                    <div className="home-item9 hidden9"><h2><Wallet size={64} /></h2></div>
+                    <div className="home-item1 hidden"><h2><Money size={64} /></h2> <h3>Save Your Money</h3></div>
+                    <div className="home-item2 hidden2"><h2><Coins size={64}/></h2><h3>Convert Coin To US Dollar</h3> </div>
+                    <div className="home-item3 hidden3"><h2><Bank size={64} /></h2><h3>FDIC Insured</h3></div>
+                    <div className="home-item4 hidden4"><h2><CurrencyBtc size={64} /></h2><h3>Invest In Bitcoin</h3></div>
+                    <div className="home-item5 hidden5"><h2><CurrencyEth size={64} /></h2><h3>Invest In Ethereum </h3></div>
+                    <div className="home-item6 hidden6"><h2><CurrencyCircleDollar size={64} /></h2><h3>Invest Your Cash</h3></div>
+                    <div className="home-item7 hidden7"><h2>< ShieldCheck size={64} /></h2><h3>Most Trusted Bank</h3></div>
+                    <div className="home-item8 hidden8"><h2><Lock size={64} /></h2><h3>Protect Your Investments</h3></div>
+                    <div className="home-item9 hidden9"><h2><Wallet size={64} /></h2><h3>Withdraw Your Money</h3></div>
 
 
                 </div>
