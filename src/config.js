2e64623b3118226f9ee957728e1e4b54a50e74f3
Francisco <figueroaf@moravian.edu>
2024-05-06 21:01:59 -0400

    used the real finance data api for my website

---
+++ src/config.js
@@ -0,0 +1,1 @@
+export const API_KEY = 'c698a6bcdemsh89f387fb6f97a7bp17cea0jsn21ab59086f0f';
--- src/pages/market/market.jsx
+++ src/pages/market/market.jsx
@@ -1,11 +1,39 @@
 import { useEffect, useState, React } from 'react'
 import './market.css'
 import axios from 'axios';
-
+import { API_KEY } from '../../config';
 const Market = () => {
   const [stocks, setStocks] = useState([]);
   const stocksToFetch = ['Apple', 'Microsoft', 'Amazon', 'Google', 'Facebook', 'Tesla', 'Netflix', 'Alphabet', 'IBM', 'Intel']; 
 
+  useEffect(() => {
+    const fetchStocks = async () => {
+      const stocksData = [];
+
+      for (const stock of stocksToFetch) {
+        const options = {
+          method: 'GET',
+          url: 'https://real-time-finance-data.p.rapidapi.com/search',
+          params: { query: stock, language: 'en' },
+          headers: {
+            'X-RapidAPI-Key': API_KEY,
+            'X-RapidAPI-Host': 'real-time-finance-data.p.rapidapi.com',
+          },
+        };
+
+        try {
+          const response = await axios.request(options);
+          stocksData.push(...response.data.data.stock.slice(0, 1));
+        } catch (error) {
+          console.error(error);
+        }
+      }
+
+      setStocks(stocksData);
+    };
+
+    fetchStocks();
+  }, []);
 
 
   return (
@@ -22,66 +50,15 @@ const Market = () => {
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>Apple</td>
-              <td>$183.38</td>
-              <td className='positive_change'>+10.35%</td>
-              <td>$173.03</td>
-            </tr>
-            <tr>
-              <td>Microsoft</td>
-              <td>$406.66</td>
-              <td className='positive_change'>+8.82%</td>
-              <td>$397.84</td>
-            </tr>
-            <tr>
-              <td>Amazon</td>
-              <td>$186.21</td>
-              <td className='positive_change'>+1.49%</td>
-              <td>$184.72</td>
-            </tr>
-            <tr>
-              <td>Google</td>
-              <td>$168.99</td>
-              <td className='positive_change'>+0.53%</td>
-              <td>$168.46</td>
-            </tr>
-            <tr>
-              <td>Facebook</td>
-              <td>$451.96</td>
-              <td className='positive_change'>+10.28%</td>
-              <td>$441.68</td>
-            </tr>
-            <tr>
-              <td>Tesla</td>
-              <td>$181.19</td>
-              <td className='positive_change'>+1.18%</td>
-              <td>$180.01</td>
-            </tr>
-            <tr>
-              <td>Draft Kings</td>
-              <td>$41.82</td>
-              <td className='negative_change'>-1.21%</td>
-              <td>$40.27</td>
-            </tr>
-            <tr>
-              <td>NVIDIA</td>
-              <td>$887.89</td>
-              <td className='positive_change'>+29.72%</td>
-              <td>$858.17</td>
-            </tr>
-            <tr>
-              <td>Netflix</td>
-              <td>$579.34</td>
-              <td className='positive_change'>+14.19%</td>
-              <td>$565.15</td>
-            </tr>
-            <tr>
-              <td>AMD</td>
-              <td>$150.60</td>
-              <td className='positive_change'>+4.44%</td>
-              <td>$146.16</td>
-            </tr>
+          {stocks.map((stock) => (
+              <tr key={stock.symbol}>
+                <td>{stock.name}</td>
+                <td>{"$" + stock.price}</td>
+                <td className={stock.change_percent > 0 ? "positive_change" : "negative_change"}>{stock.change_percent.toFixed(2) + "%"}</td>
+
+                <td>{"$"+stock.previous_close}</td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
